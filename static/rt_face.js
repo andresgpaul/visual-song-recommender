09b8411390aafd6358e6562cf575aaacaecd8fd4
Andrés González Paul Rivera <andres.g.paul@gmail.com>
2021-06-12 18:17:37 +0200

    changing io socket

--- static/rt_face.js
+++ static/rt_face.js
@@ -1,8 +1,8 @@
 const video = document.getElementById("video");
 
 
-// var socket = io.connect('http://127.0.0.1:5000');
-var socket = io.connect();
+// var socket = io.connect();
+var socket = io.connect({secure: true, transports: ["flashsocket","polling","websocket"]});
 socket.on('connect', function() {
     console.log("SOCKET CONNECTED")
 })
